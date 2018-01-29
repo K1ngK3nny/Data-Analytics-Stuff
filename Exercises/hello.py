892b7645caaa401dbaae876eb0e33462a7e353cb
K1ngK3nny <33133063+K1ngK3nny@users.noreply.github.com>
2018-01-29 17:42:19 -0600

    Week 2 HW

---
+++ Exercises/hello.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  3 11:25:07 2017
+
+@author: kenne
+"""
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None"""
+    print("Laters World")
+
+# main program starts here
+hello()
+
+
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Data-Analytics-Stuff
+Homework
